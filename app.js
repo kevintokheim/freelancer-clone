5ce906e73408883527f8a96956e1d11d24d77985
Jason Bethel & Kevin Tokheim <student@epicodus.com>
2015-09-28 16:26:02 -0700

    create header.html

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var freelancerClone = angular.module('freelancerClone', ['ui.router']);
+
+freelancerClone.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+      }
+    }
+  }); 
+});
